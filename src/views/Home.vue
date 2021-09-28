f8ac6e7da9a09efb1b41fa1130dc40b6c2b3121d
benjaminkent <benjamin.k.jehl@gmail.com>
2021-09-28 17:04:20 -0400

    base set up, make linting do what I want

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>LetterDropz</h1>
+    <h2>Hi Mom!</h2>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-});
+  name: 'Home',
+})
 </script>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
